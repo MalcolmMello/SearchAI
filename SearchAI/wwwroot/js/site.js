25a3d87fb049a84f5cc57e3b97fded0880122a46
Malcolm de Mello Lima <malcolmmellolima@gmail.com>
2024-07-10 11:22:03 -0300

    fix: wwwroot

---
+++ SearchAI/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    var data = window.location.href.substring(47);
+    var textData = document.getElementById('txtSearch');
+    textData.value = data.split("%27").join("").replace("%20", "");
+})
+
+function btnSearch() {
+    var textData = document.getElementById("txtSearch");
+
+    var url = window.location.origin;
+    url = url + "/Home/Search?searchData=" + textData.value;
+    window.location.href = url;
+}
